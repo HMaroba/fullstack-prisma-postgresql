fcab5b9b23c4561937bbbf866bfb625021ea22e3
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-19 15:53:30 +0200

    Added zod

--- src/app/api/v1/auth/register/route.ts
+++ src/app/api/v1/auth/register/route.ts
@@ -2,6 +2,18 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
 
+import { z } from "zod";
+
+// Define the schema for the request body using Zod
+const userSchema = z.object({
+  emailAddress: z.string().email({ message: "Invalid email address" }),
+  firstName: z.string().min(1, { message: "First name is required" }),
+  phoneNumber: z.string().regex(/^\d{8}$/, {
+    message: "Invalid phone number, should be 8 numeric characters",
+  }),
+  lastName: z.string().min(1, { message: "Last name is required" }),
+});
+
 export async function POST(request: Request) {
   try {
     const body = await request.json();
