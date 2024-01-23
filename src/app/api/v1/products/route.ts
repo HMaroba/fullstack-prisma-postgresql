ea2bd6008618f8e7cc08519a4a3e63b8891ba644
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-23 14:37:52 +0200

    Created schema helpers

--- src/app/api/v1/products/route.ts
+++ src/app/api/v1/products/route.ts
@@ -1,18 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../auth/register/route";
-
-import { z } from "zod";
-
-// Define the schema for the request body using Zod
-const productSchema = z.object({
-  productName: z.string().min(1, { message: "Product name is required" }),
-  description: z
-    .string()
-    .min(1, { message: "Product description is required" }),
-  image: z.string().min(1, { message: "Image is required" }),
-  price: z.number().min(1, { message: "Price is required" }),
-  userId: z.string().min(1, { message: "User id is required" }),
-});
+import { productSchema } from "@/app/helpers/schemaValidations";
 
 export async function POST(request: Request) {
   try {
--- src/app/helpers/schemaValidations.ts
+++ src/app/helpers/schemaValidations.ts
@@ -9,3 +9,14 @@ export const userSchema = z.object({
   }),
   lastName: z.string().min(1, { message: "Last name is required" }),
 });
+
+// Define the schema for the request body using Zod
+export const productSchema = z.object({
+  productName: z.string().min(1, { message: "Product name is required" }),
+  description: z
+    .string()
+    .min(1, { message: "Product description is required" }),
+  image: z.string().min(1, { message: "Image is required" }),
+  price: z.number().min(1, { message: "Price is required" }),
+  userId: z.string().min(1, { message: "User id is required" }),
+});
