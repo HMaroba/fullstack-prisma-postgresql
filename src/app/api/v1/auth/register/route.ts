2d26094155bd1cdb2466c76a300cf3e4d6d5dd06
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-23 14:34:22 +0200

    Created schema helpers

--- src/app/api/v1/auth/register/route.ts
+++ src/app/api/v1/auth/register/route.ts
@@ -1,18 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { userSchema } from "@/app/helpers/schemaValidations";
 export const prisma = new PrismaClient();
-
-import { z } from "zod";
-
-// Define the schema for the request body using Zod
-const userSchema = z.object({
-  emailAddress: z.string().email({ message: "Invalid email address" }),
-  firstName: z.string().min(1, { message: "First name is required" }),
-  phoneNumber: z.string().regex(/^\d{8}$/, {
-    message: "Invalid phone number, should be 8 numeric characters",
-  }),
-  lastName: z.string().min(1, { message: "Last name is required" }),
-});
 
 export async function POST(request: Request) {
   try {
@@ -25,7 +14,7 @@ export async function POST(request: Request) {
         { status: 400 }
       );
     }
-    
+
     const { firstName, lastName, emailAddress } = body;
     const userExists = await prisma.user.findUnique({
       where: { emailAddress },
