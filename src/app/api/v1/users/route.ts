45a7aaa5712b254f24d139ecf4cb337ed8c609e8
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-23 14:17:02 +0200

    Created get users api

--- src/app/api/v1/users/route.ts
+++ src/app/api/v1/users/route.ts
@@ -3,11 +3,11 @@ import { prisma } from "../auth/register/route";
 
 export async function GET() {
   try {
-    // const users = await prisma.user.findMany({
-    //   include: {
-    //     products: true,
-    //   },
-    // });
+    const userswithProducts = await prisma.user.findMany({
+      include: {
+        products: true,
+      },
+    });
     const users = await prisma.user.findMany();
     if (!users) {
       return NextResponse.json({
@@ -24,6 +24,38 @@ export async function GET() {
   } catch (error) {
     return NextResponse.json({
       message: "Something went wrong" + error,
+      status: 500,
+    });
+  }
+}
+
+export async function PUT(request: Request) {
+  try {
+    const body = await request.json();
+    const { firstName, lastName, phoneNumber, id } = body;
+
+    const userExists = await prisma.user.findUnique({ where: id });
+
+    if (!userExists) {
+      return NextResponse.json({
+        message: "User does not exists",
+        status: 404,
+      });
+    }
+
+    const updateUser = await prisma.user.update({
+      data: { firstName, lastName, phoneNumber },
+      where: id,
+    });
+    return NextResponse.json({
+      success: true,
+      message: "User update Successfully",
+      details: updateUser,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: "Something went wrong try again",
+      success: false,
       status: 500,
     });
   }
