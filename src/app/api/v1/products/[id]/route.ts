51301a1b46ef2315d0ac08d60dab9bde2394d8e3
HMaroba <marobahlalele910@gmail.com>
2024-01-25 12:59:06 +0200

    Added api to get product

--- src/app/api/v1/products/[id]/route.ts
+++ src/app/api/v1/products/[id]/route.ts
@@ -5,11 +5,21 @@ const prisma = new PrismaClient();
 export async function Get(params: { id: string }) {
   try {
     const { id } = params;
-    const response = await prisma.product.findUnique({ where : {id}})
+
+    const productId = await prisma.product.findUnique({ where: { id } });
+
+    if (!productId) {
+      return NextResponse.json({
+        message: "Product does not exists",
+        status: 404,
+      });
+    }
+
+    const response = await prisma.product.findUnique({ where: { id } });
     return NextResponse.json({
-        status : 200,
-        productData : response,
-    })
+      status: 200,
+      productData: response,
+    });
   } catch (error) {
     return NextResponse.json({
       message: "Something went wrong",
