bebd783c4fbb01842ceeac1a5c147f5f892fd339
HMaroba <marobahlalele910@gmail.com>
2024-01-25 14:55:55 +0200

    Fixed get product by id

--- src/app/api/v1/products/[id]/route.ts
+++ src/app/api/v1/products/[id]/route.ts
@@ -37,10 +37,16 @@ export async function GET(
   }
 }
 
-export async function PUT({ params }: { params: { id: string } }) {
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   const { id } = params;
 
   try {
+    const body = await request.json();
+
+    const {};
   } catch (error) {
     return NextResponse.json({
       message: "Something went wrong" + error,
