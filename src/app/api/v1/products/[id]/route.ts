45980b4040d48dd0a8634f38997000dab26e564a
HMaroba <marobahlalele910@gmail.com>
2024-01-25 12:56:56 +0200

    Added api to get product

--- src/app/api/v1/products/[id]/route.ts
+++ src/app/api/v1/products/[id]/route.ts
@@ -5,7 +5,11 @@ const prisma = new PrismaClient();
 export async function Get(params: { id: string }) {
   try {
     const { id } = params;
-    const response = await 
+    const response = await prisma.product.findUnique({ where : {id}})
+    return NextResponse.json({
+        status : 200,
+        productData : response,
+    })
   } catch (error) {
     return NextResponse.json({
       message: "Something went wrong",
