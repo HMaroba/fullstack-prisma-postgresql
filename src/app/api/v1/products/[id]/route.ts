32227d59d1d0912810859a837527981c7ca27587
HMaroba <marobahlalele910@gmail.com>
2024-01-25 12:55:40 +0200

    Added api to get product

--- src/app/api/v1/products/[id]/route.ts
+++ src/app/api/v1/products/[id]/route.ts
@@ -1,12 +1,15 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export async function Get() {
+export async function Get(params: { id: string }) {
   try {
-
+    const { id } = params;
+    const response = await 
   } catch (error) {
     return NextResponse.json({
-        message : 'Something went wrong',
-        status : 500,
-    })
+      message: "Something went wrong",
+      status: 500,
+    });
   }
 }
