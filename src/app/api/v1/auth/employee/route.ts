122fb47a5ceb119838ced4870903e20db4a7e5f8
HMaroba <marobahlalele910@gmail.com>
2024-02-09 09:27:20 +0200

    Created employee api

--- src/app/api/v1/auth/employee/route.ts
+++ src/app/api/v1/auth/employee/route.ts
@@ -1,8 +1,24 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../register/route";
 
 export async function Post(request: NextRequest) {
   try {
     const { first_name, last_name } = await request.json();
-  } catch (error) {}
+
+    const employeeData = await prisma.employee.create({
+      data: { first_name, last_name },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Registration Successfully",
+      status: 201,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+      status: 500,
+    });
+  }
 }
