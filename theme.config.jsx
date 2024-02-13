98f607ec4fe926bd706e9866714d69ac69564c4b
HMaroba <marobahlalele910@gmail.com>
2024-02-13 15:24:13 +0200

    added nextra

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+    logo: <span>My Nextra Documentation</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra'
+    }
+    // ... other theme options
+  }
